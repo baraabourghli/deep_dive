a59eb25694a1207159e1618d0598d9af799492a8
baraabourghli <baraa@mindvalley.com>
2012-12-16 23:21:35 +0800

    creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeepDive::Application.config.secret_token = '3a6ad55931ee7940bd0d2f308cb19895c2ef768408e66827861ac71b3e7ccc06c32e38e795f79bf9eb7092e27b57347f03816b3b2158fcf04aa2345ac3213db2'
